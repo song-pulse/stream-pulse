f9bc977246ae5123972f237fb367e1ade3d299de
Dimitri Kohler <dimitri.kohler@bf.uzh.ch>
2020-03-24 14:29:08 +0100

    added start_stop button

--- src/components/entry.js
+++ src/components/entry.js
@@ -1,12 +1,23 @@
 import React from "react"
+import axios from "axios"
 
 function Entry(props) {
   let data = props.data;
   return (
     <div style={{border:"1px solid black", padding:"1.5em"}}>
-      <p style={{display:"inline"}}>{data.running ? "x Running" : "> Waiting"}</p> <p style={{display:"inline"}}>{data.timeRunning}/{data.timeTotal}</p> <p style={{display:"inline"}}>{data.participantId}</p> <p style={{display:"inline"}}>{data.song}</p>
+      <button onClick={() => start_stop_simulation(data.id, !data.running)}>{data.running ? "x STOP" : "> RUN"}</button>
+      <p style={{display:"inline"}}> {"  "+data.timeRunning}/{data.timeTotal}</p>
+      <p style={{display:"inline"}}> {"  "+data.participantId}</p>
+      <p style={{display:"inline"}}>{"  "+data.song}</p>
     </div>
   )
+}
+
+function start_stop_simulation(id, start_stop) {
+  axios.put(process.env.GATSBY_API_URL+"simulations/" + id, {start_stop: start_stop})
+    .then(function(response) {
+      console.log(response);
+  });
 }
 
 export default Entry
--- src/components/upload.js
+++ src/components/upload.js
@@ -3,9 +3,9 @@ import React from "react"
 const Upload = () => (
     <>
       <form method="post" action={process.env.GATSBY_API_URL+"simulations"}>
-        <label htmlFor={"id"}>Participant:</label>
+        <label htmlFor={"id"}>Participant: </label>
         <input type="text" id="id" name="id"/>
-        <label htmlFor={"file"}> Data:</label>
+        <label htmlFor={"file"}> Data: </label>
         <input type="file" id="file" name="file"/>
         <input type="submit" value="Submit"/>
       </form>
