819a3c665c1bc042ca26fb4f5e9c6ad097f4af66
Dimitri Kohler <dimitri.kohler@bf.uzh.ch>
2020-04-07 17:37:09 +0200

    updated to display the new table structure

--- src/components/entry.js
+++ src/components/entry.js
@@ -3,12 +3,22 @@ import axios from "axios"
 
 function Entry(props) {
   let data = props.data;
+  let run = data.runs.find(run => run.is_running)
+  let is_running = false
+  let song = ""
+  if (run) {
+    is_running = true
+    let current_timestamp = Math.max.apply(Math, run.results.map(function(o) { return o.timestamp; }))
+    let current_result = run.results.find(function(o){ return o.timestamp === current_timestamp; })
+    console.log(current_result)
+    song = current_result ? current_result.song : "NOT RUNNING"
+  }
   return (
     <div style={{border:"1px solid black", padding:"1.5em", marginTop:"0.5em"}}>
-      <button onClick={() => start_stop_simulation(data.id, !data.running)}>{data.running ? "x STOP" : "> RUN"}</button>
-      <p style={{display:"inline"}}> {"  "+data.timeRunning}/{data.timeTotal}</p>
-      <p style={{display:"inline"}}> {"  "+data.participantId}</p>
-      <p style={{display:"inline"}}>{"  "+data.song}</p>
+      <button onClick={() => start_stop_simulation(data.id, !is_running)}>{is_running ? "x STOP" : "> RUN"}</button>
+      <p style={{display:"inline"}}> {"  "+ (run ? run.current_time: 0)}/{data.total_time}</p>
+      <p style={{display:"inline"}}> {"  "+ data.participant_id}</p>
+      <p style={{display:"inline"}}>{"  "+ song}</p>
     </div>
   )
 }
