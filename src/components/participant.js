98eef219739b1a00ab39efaa0f2dc81936303200
Dimitri Kohler <dimitri.kohler@bf.uzh.ch>
2020-04-09 10:33:11 +0200

    buttons to add and delete participants

--- src/components/participant.js
+++ src/components/participant.js
@@ -2,14 +2,22 @@ import React from "react"
 import Paper from '@material-ui/core/Paper'
 import Recording from "./recording"
 import Button from "@material-ui/core/Button"
+import axios from "axios"
 
-function Participant(props) {
+const Participant = (props) => {
   let data = props.data;
   return (
     <Paper style={{border:"1px solid black", padding:"0.5em", marginTop:"0.5em"}}>
       <Button>{data.name}</Button>
+      <Button onClick={() => deleteParticipant(data.id)}>Delete</Button>
       {data.recordings.map(rec => <Recording key={rec.id} data={rec}/>)}
     </Paper>)
+}
+
+const deleteParticipant = (id) => {
+  axios.delete(process.env.GATSBY_API_URL+"participants/"+id)
+    .then((response) => {console.log(response)})
+    .catch((error) => {console.log(error)});
 }
 
 export default Participant
--- src/components/upload.js
+++ src/components/upload.js
@@ -1,19 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
+import TextField from "@material-ui/core/TextField"
+import Button from "@material-ui/core/Button"
+import axios from "axios"
 
-const Upload = () => (
+const Upload = () => {
+  const [name, setName] = useState("");
+  return (
     <>
-      <form method="post" action={process.env.GATSBY_API_URL+"simulations"}>
-        <label htmlFor={"id"}>Participant: </label>
-        <input type="text" id="id" name="id"/>
-        <label htmlFor={"EDA"}> EDA: </label>
-        <input type="file" id="eda" name="eda"/>
-        <label htmlFor={"EDA"}> HR: </label>
-        <input type="file" id="hr" name="hr"/>
-        <label htmlFor={"EDA"}> Gyro: </label>
-        <input type="file" id="gyro" name="gyro"/>
-        <input type="submit" value="Submit"/>
-      </form>
+      <TextField required label="New Participant" variant="outlined" size={"small"} onChange={e => setName(e.target.value)} value={name}/>
+      <Button onClick={() => name ? createUser(name) : ""} variant="contained" color={"primary"} type="submit" style={{marginLeft:"10px"}}>Add</Button>
     </>
-)
+  )
+}
+
+const createUser = (name) => {
+  axios.post(process.env.GATSBY_API_URL+"participants", { name: name })
+    .then((response) => {console.log(response)})
+    .catch((error) => {console.log(error)})
+}
 
 export default Upload
