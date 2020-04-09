3627dcc5da52bbf1c496b9df805a2e7b0f30cd6c
Dimitri Kohler <dimitri.kohler@bf.uzh.ch>
2020-04-09 14:43:08 +0200

    test commit

--- src/components/participant.js
+++ src/components/participant.js
@@ -2,14 +2,18 @@ import React from "react"
 import Paper from '@material-ui/core/Paper'
 import Recording from "./recording"
 import Button from "@material-ui/core/Button"
+import Box from "@material-ui/core/Box"
 import axios from "axios"
 
 const Participant = (props) => {
   let data = props.data;
   return (
     <Paper style={{border:"1px solid black", padding:"0.5em", marginTop:"0.5em"}}>
-      <Button>{data.name}</Button>
-      <Button onClick={() => deleteParticipant(data.id)}>Delete</Button>
+      <Box style={{display: "flex", justifyContent: "space-between"}}>
+        <Button>{data.name}</Button>
+        <Button onClick={() => deleteParticipant(data.id)}>Delete</Button>
+      </Box>
+
       {data.recordings.map(rec => <Recording key={rec.id} data={rec}/>)}
     </Paper>)
 }
