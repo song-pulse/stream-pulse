aad15ba0ffac6d0037e48456051f554cb41c53a3
Anja Koller <anja.koller@uzh.ch>
2020-05-08 14:37:30 +0200

    Add ACC visualization with 3 values

--- src/components/file/cleanup.js
+++ src/components/file/cleanup.js
@@ -21,11 +21,48 @@ const cleanupSingleColumn = async (data, file_id, timeWindow, createValue) => {
 const cleanupTwoColumn = async (data, file_id, createValue) => {
   console.log('2 COL DATA')
   console.log(data)
-  // TODO Anja: HRV would be two cols
+  // TODO Anja: IBI would be two cols
 }
 
 const cleanupThreeColumn = async (data, file_id, timeWindow, createValue) => {
+  console.log('3 COL DATA')
+  let frequency = parseInt(data[1][0]) // 32
+  let timestamp = parseInt(data[0][1])
+  let stepWindow = timeWindow * frequency // should be 180*32= 5760
+  console.log('STEPWINDOW', stepWindow)
+  console.log('DATALENGHT', data.length) // 884661
+  let sumX=0
+  let sumY=0
+  let sumZ=0
+
+  for (let win = 2; win < data.length - stepWindow; win = win + stepWindow) {
+    for (let winStart = win; winStart < win + stepWindow; winStart++) {
+      let valueX = parseFloat(data[winStart][0])
+      let valueY = parseFloat(data[winStart][1])
+      let valueZ = parseFloat(data[winStart][2])
+      sumX += valueX
+      sumY += valueY
+      sumZ += valueZ
+    }
+    timestamp = timestamp + timeWindow
+    let avgX = sumX / stepWindow
+    let avgY = sumY / stepWindow
+    let avgZ = sumZ / stepWindow
+    // TODO: avg[win] is the summed value with all three values v1,v2,v3 in one 
+    // if we want everyone for his own we do not have to do the buff and sum thing
+    let res = await createValue(timestamp, avgX, avgY, avgZ, file_id)
+    console.log('res   avgX   avgY   avgZ',res + " " + avgX + " "+ avgY + " " + avgZ)
+    sumX = 0
+    sumY = 0
+    sumZ = 0
+  }
+  return true; // TODO: handle errors
+}
+
+
+const cleanupThreeColumnAvg = async (data, file_id, timeWindow, createValue) => {
   /**
+   * TODO: this method is not used yet, as the avg gets computed in the preprocessingstep
    * timestamp=data[0]: [1585038955,1585038955, 1585038955], frequency=data[1]: [32,32,32], acc=data[2]: [33,-3,54]
    * https://support.empatica.com/hc/en-us/articles/202028739-How-is-the-acceleration-data-formatted-in-E4-connect-
    * sum+= max3(abs(buffX[i] - prevX), abs(buffY[i] - prevY), abs(buffZ[i] - prevZ))
@@ -34,7 +71,7 @@ const cleanupThreeColumn = async (data, file_id, timeWindow, createValue) => {
   console.log('3 COL DATA')
   let frequency = parseInt(data[1][0]) // 32
   let timestamp = parseInt(data[0][1])
-  let stepWindow = timeWindow * frequency // should be 180*32
+  let stepWindow = timeWindow * frequency // should be 180*32= 5760
   console.log('STEPWINDOW', stepWindow)
   console.log('DATALENGHT', data.length) // 884661
 
