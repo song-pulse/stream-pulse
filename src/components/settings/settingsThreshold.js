4648c32ef8068e87ce76dd23c07468b9c4cfdbcd
Dimitri Kohler <dimitri.kohler@bf.uzh.ch>
2020-05-28 10:46:29 +0200

    added prr_threshold to settings

--- src/components/settings/settingsThreshold.js
+++ src/components/settings/settingsThreshold.js
@@ -12,6 +12,7 @@ function SettingsThreshold() {
   const [temp_baseline, setTempBaseline] = useState(0.0)
   const [eda_threshold, setEDAThreshold] = useState(0.0)
   const [ibi_threshold, setIbiThreshold] = useState(0.0)
+  const [prr_threshold, setPrrThreshold] = useState(0.0)
   const [temp_latency, setTempLatency] = useState(0)
   const [duration, setDuration] = useState(0)
   const [loaded, isLoaded] = useState(false)
@@ -24,8 +25,9 @@ function SettingsThreshold() {
          setAccThreshold(response.data.acc_threshold)
          setEDAThreshold(response.data.eda_threshold)
          setTempBaseline(response.data.temp_baseline)
-         setIbiThreshold(response.data.ibi_threshold)
-         setTempLatency(response.data.temp_latency)
+          setIbiThreshold(response.data.ibi_threshold)
+          setPrrThreshold(response.data.prr_threshold)
+          setTempLatency(response.data.temp_latency)
          setDuration(response.data.duration)
           isLoaded(true)
         })
@@ -33,8 +35,16 @@ function SettingsThreshold() {
 
     const update = (event) => {
       event.preventDefault()
-      axios.put(process.env.GATSBY_API_URL + "settings", {stress_threshold: stress_threshold, acc_threshold: acc_threshold, eda_threshold: eda_threshold,
-                ibi_threshold: ibi_threshold, temp_baseline: temp_baseline, temp_latency: temp_latency, duration: duration })
+      axios.put(process.env.GATSBY_API_URL + "settings", {
+        stress_threshold: stress_threshold,
+        acc_threshold: acc_threshold,
+        eda_threshold: eda_threshold,
+        ibi_threshold: ibi_threshold,
+        prr_threshold: prr_threshold,
+        temp_baseline: temp_baseline,
+        temp_latency: temp_latency,
+        duration: duration,
+      })
     }
 
     if (!loaded) { // only load once
@@ -57,23 +67,32 @@ function SettingsThreshold() {
             </ListItem>
             <ListItem>
               <TextField required label="Temp Baseline" variant="outlined" size={"small"} onChange={(s) => setTempBaseline(s.target.value)}
-                value={temp_baseline}/>
+                         value={temp_baseline}/>
             </ListItem>
             <ListItem>
-              <TextField required label="EDA Threshold" variant="outlined" size={"small"} onChange={(s) => setEDAThreshold(s.target.value)}
-                value={eda_threshold}/>
+              <TextField required label="EDA Threshold" variant="outlined" size={"small"}
+                         onChange={(s) => setEDAThreshold(s.target.value)}
+                         value={eda_threshold}/>
             </ListItem>
             <ListItem>
-              <TextField required label="IBI Threshold" variant="outlined" size={"small"} onChange={(s) => setIbiThreshold(s.target.value)}
-                value={ibi_threshold}/>
+              <TextField required label="IBI Threshold" variant="outlined" size={"small"}
+                         onChange={(s) => setIbiThreshold(s.target.value)}
+                         value={ibi_threshold}/>
             </ListItem>
             <ListItem>
-              <TextField required label="Temp Latency" variant="outlined" size={"small"} onChange={(s) => setTempLatency(s.target.value)}
-                value={temp_latency}/>
+              <TextField required label="PRR Threshold" variant="outlined" size={"small"}
+                         onChange={(s) => setPrrThreshold(s.target.value)}
+                         value={prr_threshold}/>
             </ListItem>
             <ListItem>
-              <TextField required label="Duration" variant="outlined" size={"small"} onChange={(s) => setDuration(s.target.value)}
-                value={duration}/>
+              <TextField required label="Temp Latency" variant="outlined" size={"small"}
+                         onChange={(s) => setTempLatency(s.target.value)}
+                         value={temp_latency}/>
+            </ListItem>
+            <ListItem>
+              <TextField required label="Duration" variant="outlined" size={"small"}
+                         onChange={(s) => setDuration(s.target.value)}
+                         value={duration}/>
             </ListItem>
             <ListItem>
               <Button variant="contained" color={"primary"} type="submit" style={{ marginLeft: "10px" }}>Update</Button>
